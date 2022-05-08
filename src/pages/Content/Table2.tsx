ed075804d91b0a773b9d67d1c2cdf7e2b1701192
zhongshizhi <zhongshizhi@cdzhiyong.com>
2022-05-08 19:01:57 +0800

    '2'

---
+++ src/pages/Content/Table2.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './styles.less';
+export default function Table2(props){
+
+
+    return (
+
+    )
+}
--- src/pages/Content/index.tsx
+++ src/pages/Content/index.tsx
@@ -1,15 +1,36 @@
 import React, { forwardRef } from 'react';
 import './styles.less';
-
+import dayjs from "dayjs";
 export default forwardRef(function Content(props, ref){
 
 
     return (
         <div className='content-div' ref={ref}>
            <h2>温江佳茂轮胎经营部出货单</h2>
-            <table>
-
-            </table>
+            <div className={'table'}>
+                <div className={'line_1'}>
+                    <span>
+                        购货单位(个人) :
+                    </span>
+                    <span>
+                        联系人 :
+                    </span>
+                    <span>
+                        单号:
+                    </span>
+                </div>
+                <div className={'line_1'}>
+                    <span>
+                        地址 :
+                    </span>
+                    <span>
+                        联系方式 :
+                    </span>
+                    <span>
+                        打印日期 : {dayjs().format('YYYY-MM-DD HH:mm:ss')}
+                    </span>
+                </div>
+            </div>
         </div>
     )
 })
